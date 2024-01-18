89db37fb9810c8446338289405dac2b5d6a2cc9a
Minjeong63 <kmj06030@naver.com>
2024-01-18 15:24:39 +0900

    Feat: mysql 연결

---
+++ src/test/java/qt/qr_backend/QrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package qt.qr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
