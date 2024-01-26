fc20e0133aa17f9dfe2ec396574517062a4566dd
Minjeong63 <kmj06030@naver.com>
2024-01-26 22:22:52 +0900

    Feat: Ceo, Store 생성자 생성 둘 다 기본 생성자 + 회원가입에서 필요한 생성자 생성 Store Entity의 address 필드를 mainAddress + subAddress로 분리

--- src/main/java/qt/qr_backend/domain/Ceo.java
+++ src/main/java/qt/qr_backend/domain/Ceo.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class Ceo {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ceo_id")
@@ -21,4 +25,16 @@ public class Ceo {
     private String accountNumber;
     private String email;
     private String bank;
+
+    public Ceo(String name, String mobileNumber, String loginId, String password, boolean isAdmin, String bank, String accountNumber,
+               String email) {
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+        this.loginId = loginId;
+        this.password = password;
+        this.isAdmin = isAdmin;
+        this.bank = bank;
+        this.accountNumber = accountNumber;
+        this.email = email;
+    }
 }
