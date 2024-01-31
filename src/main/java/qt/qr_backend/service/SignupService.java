1d9d5e6bcae0f531cf14c7036bef5a2981121b3d
Minjeong63 <kmj06030@naver.com>
2024-01-31 20:10:53 +0900

    Feat: 데이터 검증 기능 추가 아이디 중복, 사업자등록번호 중복

--- src/main/java/qt/qr_backend/service/SignupService.java
+++ src/main/java/qt/qr_backend/service/SignupService.java
@@ -9,6 +9,7 @@ import qt.qr_backend.controller.request.CeoSignupRequest;
 import qt.qr_backend.controller.request.StoreSignupRequest;
 import qt.qr_backend.domain.Ceo;
 import qt.qr_backend.domain.Store;
+import qt.qr_backend.exception.DuplicateException;
 import qt.qr_backend.repository.CeoRepository;
 import qt.qr_backend.repository.StoreRepository;
 
@@ -24,17 +25,37 @@ public class SignupService {
     @Transactional
     public void signup(CeoSignupRequest ceoRequest, StoreSignupRequest storeRequest) {
         log.info("Ceo 생성 시작");
+        validateDuplicateLoginId(ceoRequest.getLoginId());
         Ceo ceo = new Ceo(ceoRequest.getName(), ceoRequest.getMobileNumber(), ceoRequest.getLoginId(),
                 ceoRequest.getIsAdmin(), ceoRequest.getBank(), ceoRequest.getAccountNumber(),
                 ceoRequest.getEmail());
-
         ceo.encodePassword(passwordEncoder, ceoRequest.getPassword());
         ceoRepository.save(ceo);
+        log.info("Ceo 생성 완료");
 
+        log.info("Store 생성 시작");
+        validateDuplicateBusinessNumber(storeRequest.getBusinessNumber());
         Store store = new Store(ceo, storeRequest.getName(), storeRequest.getPhoneNumber(),
                 storeRequest.getMainAddress(),
                 storeRequest.getDetailAddress(), storeRequest.getBusinessNumber(), storeRequest.getTableCount(),
                 storeRequest.getApproval());
         storeRepository.save(store);
+        log.info("Store 생성 완료");
+    }
+
+    private void validateDuplicateLoginId(String loginId) {
+        boolean existLoginId = ceoRepository.findByLoginId(loginId)
+                .stream().findAny().isPresent();
+        if (existLoginId) {
+            throw new DuplicateException("이미 가입된 아이디입니다.");
+        }
+    }
+
+    private void validateDuplicateBusinessNumber(String businessNumber) {
+        boolean existBusinessNumber = storeRepository.findByBusinessNumber(businessNumber)
+                .stream().findAny().isPresent();
+        if (existBusinessNumber) {
+            throw new DuplicateException("이미 가입된 사업자등록번호입니다.");
+        }
     }
 }
