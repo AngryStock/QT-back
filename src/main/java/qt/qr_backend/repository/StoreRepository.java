78361ecfc23ed05e955dc5e51c26d600381df20f
goodnight <qjadjehd05@gmail.com>
2024-04-17 21:37:32 +0900

    Fix: Store 리포지토리 오류 수정본

--- src/main/java/qt/qr_backend/repository/StoreRepository.java
+++ src/main/java/qt/qr_backend/repository/StoreRepository.java
@@ -3,30 +3,26 @@ package qt.qr_backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import qt.qr_backend.domain.Ceo;
 import qt.qr_backend.domain.Category;
 import qt.qr_backend.domain.Store;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
-
 public interface StoreRepository extends JpaRepository<Store,String> {
+
+    Optional<Store> findById(String id);
+
     List<Store> findByCeo_Id(String ceoId);
 
     @Query("select c from Category c join fetch c.store s join fetch s.ceo ce where c.id= :categoryId")
     Category findNoProxyCategoryById(String categoryId);
 
-    public List<Store> findByBusinessNumber(String businessNumber) {
-        return em.createQuery("select s from Store s where s.businessNumber = :businessNumber", Store.class)
-                .setParameter("businessNumber", businessNumber)
-                .getResultList();
-    }
+    List<Store> findByBusinessNumber(String businessNumber);
 
     // 모든 Store 정보 조회 - AdminService
-    public List<Store> findAll() {
-        return em.createQuery("select s from Store s", Store.class).getResultList();
-    }
-    List<Store> findByBusinessNumber(String businessNumber);
+    @Query("select s from Store s")
+    List<Store> findAll();
 }
