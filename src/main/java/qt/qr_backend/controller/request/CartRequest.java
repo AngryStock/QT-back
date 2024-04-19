8cad0ab7844e0718f26cf0341e037e8574cfd944
pss240 <arp11150@gmail.com>
2024-04-19 19:03:37 +0900

    Feet:del, alldel Cart 추가

--- src/main/java/qt/qr_backend/controller/request/CartRequest.java
+++ src/main/java/qt/qr_backend/controller/request/CartRequest.java
@@ -14,4 +14,24 @@ public class CartRequest {
     private CartDTO cartDTO;
     private String cartId;
     private int amount;
+
+    public CartRequest(String add, CartDTO cartDTO) {
+        this.type = add;
+        this.cartDTO = cartDTO;
+    }
+
+    public CartRequest(String set, String cartId, int amount) {
+        this.type = set;
+        this.cartId = cartId;
+        this.amount= amount;
+    }
+
+    public CartRequest(String del, String cartId) {
+        this.type = del;
+        this.cartId = cartId;
+    }
+
+    public CartRequest(String allDel) {
+        this.type = allDel;
+    }
 }
