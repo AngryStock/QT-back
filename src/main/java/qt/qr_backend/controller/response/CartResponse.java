e4debbef1c0882db17a8d3c84bf72350d20470e8
pss240 <arp11150@gmail.com>
2024-04-19 21:29:04 +0900

    Fix: del, allDel Cart

--- src/main/java/qt/qr_backend/controller/response/CartResponse.java
+++ src/main/java/qt/qr_backend/controller/response/CartResponse.java
@@ -1,12 +1,38 @@
 package qt.qr_backend.controller.response;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.*;
+import qt.qr_backend.DTO.CartDTO;
 
 @AllArgsConstructor
-@Getter
+@NoArgsConstructor
+
+@Data
 public class CartResponse {
-    private int statusCode;
+    private String type;
+    private CartDTO cartDTO;
+    private String cartId;
+    private int amount;
     private String message;
+    private int statusCode;
+
+    public CartResponse(String add, CartDTO cartDTO) {
+        this.type = add;
+        this.cartDTO = cartDTO;
+    }
+
+    public CartResponse(String set, String cartId, int amount) {
+        this.type = set;
+        this.cartId = cartId;
+        this.amount = amount;
+    }
+
+    public CartResponse(String del, String cartId) {
+        this.type = del;
+        this.cartId = cartId;
+    }
+
+    public CartResponse(String allDel) {
+        this.type = allDel;
+    }
 }
